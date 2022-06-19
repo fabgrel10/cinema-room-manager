e702781cca9905b30712e14c9311a801a73561fa
Fabrizio <fabgrel10@gmail.com>
2022-06-18 23:03:18 -0500

    ✅ Status correct

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,6 +1,10 @@
 
 public class Cinema {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for (int i = 1; i < 8; i++) {
+            System.out.printf("%d S S S S S S S S\n", i);
+        }
     }
 }
